be6f597df6b2c27a63dc0bff903b307cc38e8257
AYUSH SINGLA <54733520+ayu913@users.noreply.github.com>
2021-02-23 00:14:53 +0530

    Initial Commit

---
+++ firstapp/src/index.js
@@ -0,0 +1,25 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById("root"));
+
+ReactDOM.render(
+  <>
+    <h1> Social Media Applications </h1>
+
+    <ol>
+      <li> Instagram </li>
+      <li> Whatsapp </li>
+      <li> Snapchat </li>
+      <li> Telegram </li>
+      <li> Facebook </li>
+      
+    </ol>
+  </>,
+
+  document.getElementById("root")
+);
