5a052bdea38cc81d1d6229a1d0bf2587606e9cae
AYUSH SINGLA <54733520+ayu913@users.noreply.github.com>
2021-03-15 16:03:46 +0530

    Initial Commit

---
+++ github-user-data-hooks/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react'
+import UseEffectAPI from './components/useEffect/useReactAPI';
+
+const App = () => {
+  return (
+    <div>
+      <UseEffectAPI/>
+    </div>
+  )
+}
+
+export default App;
